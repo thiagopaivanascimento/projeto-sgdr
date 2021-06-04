f1fce169afecef8d56ff504bdd6b5a3b6fe79b0d
Thiago de Paiva Nascimento <thiagodepaivainfo@gmail.com>
2021-06-04 13:30:53 -0300

    Atualizacao BD

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,9 +1,9 @@
 <?
 
-if ($conexao = pg_connect ("host=localhost user=postgres port=5432 dbname=SGDR password=post")){
+if ($conexao = pg_connect ("host=localhost user=postgres port=5432 dbname=bd_sgdr password=post")){
  echo '';
  }else{
-   echo 'FALHA NA CONEXÃO!';
+   echo 'FALHA NA CONEXÃƒO!';
    }
 
 ?>
